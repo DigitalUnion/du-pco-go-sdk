16b16fa22e149732beadb1a3c22543b9d1243159
chenwei <chenwei@shuzilm.cn>
2023-03-21 12:11:18 +0800

    example

--- example/client.go
+++ example/client.go
@@ -4,7 +4,7 @@
  * @Date: 2022/09/09 2:34 PM
  */
 
-package example
+package main
 
 import (
 	"github.com/DigitalUnion/du-pco-go-sdk/dupco"
@@ -12,9 +12,13 @@ import (
 	"log"
 )
 
+func main() {
+	ExampleClient()
+}
+
 func ExampleClient() {
 	api := dupco.NewDataClient("cloud-test", "aa", "yDpDEihpUsF_RyWsCES1H")
-	api.EnableTestMode()
+	//api.EnableTestMode()
 	for i := 0; i < 10; i++ {
 		r := api.Call("idmap-query-all", []byte(`{"f":"mac,imei","k":"868862032205613","m":"0"}`))
 		j, _ := json.Marshal(r)
